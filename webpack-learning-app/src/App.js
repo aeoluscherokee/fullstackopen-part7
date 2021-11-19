11abe6d0932b3892591ff9caf159bc5c46b819f3
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-20 00:20:34 +0700

    Store previos value of counter

--- webpack-learning-app/src/App.js
+++ webpack-learning-app/src/App.js
@@ -2,11 +2,19 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  const [values, setValues] = useState();
 
   return (
     <div className="container">
       hello webpack {counter} clicks
-      <button onClick={() => setCounter(counter + 1)}>press</button>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+          setValues(values.concat(counter));
+        }}
+      >
+        press
+      </button>
     </div>
   );
 };
