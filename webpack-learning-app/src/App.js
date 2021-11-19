31a4463e255ead12ba7e63af74cfbc6842dd9f02
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-20 00:35:05 +0700

    Create custom hook to fetch notes

--- webpack-learning-app/src/App.js
+++ webpack-learning-app/src/App.js
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useNotes = (url) => {
+  const [notes, setNotes] = useState([]);
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setNotes(response.data);
+    });
+  }, [url]);
+  return notes;
+};
 
 const App = () => {
   const [counter, setCounter] = useState(0);
   const [values, setValues] = useState([]);
+  const url = 'http://localhost:3001/notes';
+  const notes = useNotes(url);
 
   return (
     <div className="container">
@@ -15,6 +28,9 @@ const App = () => {
       >
         press
       </button>
+      <div>
+        {notes.length} notes on server {url}
+      </div>
     </div>
   );
 };
