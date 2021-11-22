1183ba780815f6c44601aa1d74604f90571aff63
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-22 18:02:06 +0700

    Modify SwitchCase rule

--- bloglists/.eslintrc.js
+++ bloglists/.eslintrc.js
@@ -14,7 +14,14 @@ module.exports = {
   },
   plugins: ['react', 'jest'],
   rules: {
-    indent: ['error', 2],
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+        ignoredNodes: ['ConditionalExpression'],
+      },
+    ],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     eqeqeq: 'error',
