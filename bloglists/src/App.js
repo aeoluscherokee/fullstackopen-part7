086cfd25db1e78158097f8f8091f1e20b3c943d7
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-22 18:07:58 +0700

    Refactor App component

--- bloglists/src/App.js
+++ bloglists/src/App.js
@@ -1,22 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Blog from './components/Blog';
 import blogService from './services/blogs';
-import userService from './services/users';
-import Notification from './components/Notification';
 import LogIn from './components/LogIn';
 import CreateNewBlog from './components/CreateNewBlog';
 import Togglable from './components/Togglable';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { showNotification } from './reducers/notificationReducer';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
-  const [loginData, setLoginData] = useState({ username: '', password: '' });
-  const [userData, setUserData] = useState({
-    token: '',
-    username: '',
-    name: '',
-  });
+  const userData = useSelector(({ user }) => user);
   const blogFormRef = useRef();
 
   useEffect(() => {
@@ -29,22 +22,6 @@ const App = () => {
   }, []);
 
   const dispatch = useDispatch();
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await userService.logIn(loginData);
-      setUserData(response);
-      setLoginData({ username: '', password: '' });
-    } catch (error) {
-      dispatch(
-        showNotification(
-          { style: 'error', message: error.response.data.error },
-          3
-        )
-      );
-      setLoginData({ username: '', password: '' });
-    }
-  };
   const addBlog = async (newBlog) => {
     try {
       const response = await blogService.createNewBlog(newBlog, userData.token);
@@ -72,11 +49,6 @@ const App = () => {
   };
   const cancelAddBlog = () => blogFormRef.current.toggleVisibility();
 
-  const handleOnChange = (e) => {
-    setLoginData((prev) => {
-      return { ...prev, [e.target.name]: e.target.value };
-    });
-  };
   const handleUpdateLike = async (blog) => {
     const updatedBlog = {
       user: blog.user.id,
@@ -122,7 +94,7 @@ const App = () => {
           showNotification(
             {
               style: 'error',
-              message: `a blog ${title}is not existed`,
+              message: `a blog ${title} is not existed`,
             },
             3
           )
@@ -131,43 +103,23 @@ const App = () => {
     } else return;
   };
 
-  const handleLogout = () => {
-    setUserData({});
-  };
   return (
-    <div>
-      {userData.username ? (
-        <div>
-          <h2>blogs</h2>
-          <div>
-            <Notification notification={notification} />
-          </div>
-          <p>
-            {userData.name} logged in
-            <button onClick={handleLogout}>logout</button>
-          </p>
-          <Togglable showLabel="create new blog" ref={blogFormRef}>
-            <CreateNewBlog addBlog={addBlog} cancelAddBlog={cancelAddBlog} />
-          </Togglable>
-
-          {blogs.map((blog) => (
-            <Blog
-              key={blog.id}
-              blog={blog}
-              updateLike={handleUpdateLike}
-              deleteBlog={handleDelete}
-              user={userData}
-            />
-          ))}
-        </div>
-      ) : (
-        <LogIn
-          handleSubmit={handleSubmit}
-          handleOnChange={handleOnChange}
-          loginData={loginData}
-        />
-      )}
-    </div>
+    <>
+      <LogIn>
+        <Togglable showLabel="create new blog" ref={blogFormRef}>
+          <CreateNewBlog addBlog={addBlog} cancelAddBlog={cancelAddBlog} />
+        </Togglable>
+        {blogs.map((blog) => (
+          <Blog
+            key={blog.id}
+            blog={blog}
+            updateLike={handleUpdateLike}
+            deleteBlog={handleDelete}
+            user={userData}
+          />
+        ))}
+      </LogIn>
+    </>
   );
 };
 
