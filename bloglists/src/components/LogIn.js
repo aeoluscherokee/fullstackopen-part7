b29693fc8a135344a7315d3bea85e6294f51f0c3
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-22 18:07:11 +0700

    Modify Login component using redux

--- bloglists/src/components/LogIn.js
+++ bloglists/src/components/LogIn.js
@@ -1,39 +1,71 @@
 import React from 'react';
 import Notification from './Notification';
-import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { useField } from '../hooks';
+import { showNotification } from '../reducers/notificationReducer';
+import { login, logout } from '../reducers/userReducer';
+import userService from '../services/users';
 
-const Login = ({ notification, handleSubmit, handleOnChange, loginData }) => {
+const Login = (props) => {
+  const username = useField('text');
+  const password = useField('password');
+  const userData = useSelector(({ user }) => user);
+  const dispatch = useDispatch();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const loginData = {
+      username: username.value,
+      password: password.value,
+    };
+    try {
+      const response = await userService.logIn(loginData);
+      dispatch(login(response));
+    } catch (error) {
+      dispatch(
+        showNotification(
+          { style: 'error', message: error.response.data.error },
+          3
+        )
+      );
+      username.onReset();
+      password.onReset();
+    }
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   return (
-    <div>
-      <h1>log in to application</h1>
-      <Notification notification={notification} />
-      <form onSubmit={handleSubmit}>
-        username
-        <input
-          id="username"
-          name="username"
-          onChange={handleOnChange}
-          value={loginData.username}
-        ></input>
-        password
-        <input
-          id="password"
-          type="password"
-          name="password"
-          onChange={handleOnChange}
-          value={loginData.password}
-        ></input>
-        <button type="submit">submit</button>
-      </form>
-    </div>
+    <>
+      {userData.username ? (
+        <div>
+          <h2>blogs</h2>
+          <div>
+            <Notification />
+          </div>
+          <p>
+            {userData.name} logged in
+            <button onClick={handleLogout}>logout</button>
+          </p>
+          {props.children}
+        </div>
+      ) : (
+        <div>
+          <h1>log in to application</h1>
+          <Notification />
+          <form onSubmit={handleSubmit}>
+            username
+            <input id="username" name="username" {...username}></input>
+            password
+            <input id="password" name="password" {...password}></input>
+            <button type="submit">submit</button>
+          </form>
+        </div>
+      )}
+    </>
   );
-};
-
-Login.propTypes = {
-  notification: PropTypes.object.isRequired,
-  handleSubmit: PropTypes.func.isRequired,
-  handleOnChange: PropTypes.func.isRequired,
-  loginData: PropTypes.object.isRequired,
 };
 
 export default Login;
