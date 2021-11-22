35033d541f9dde1bf419f4254b625a010b76bf11
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-22 18:33:32 +0700

    Create blog reducer with action creator

---
+++ bloglists/src/reducers/blogReducer.js
@@ -0,0 +1,24 @@
+import blogService from '../services/blogs';
+
+const initialState = [];
+
+const blogReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'NEW_BLOG':
+      return [...state, action.data];
+    case 'ALL_BLOGS':
+      return action.data;
+    default:
+      return state;
+  }
+};
+
+export const getAllBlogs = () => {
+  return async (dispatch) => {
+    const blogs = await blogService.getAll();
+    const sortedBlogs = blogService.sortBlogs(blogs);
+    dispatch({ type: 'ALL_BLOGS', data: sortedBlogs });
+  };
+};
+
+export default blogReducer;
