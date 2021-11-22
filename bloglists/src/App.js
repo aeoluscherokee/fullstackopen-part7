1a8228e8328c78c574558373665fc7580200a97d
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-22 23:28:35 +0700

    Remove update like functionality from App

--- bloglists/src/App.js
+++ bloglists/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Blog from './components/Blog';
-import blogService from './services/blogs';
 import LogIn from './components/LogIn';
 import CreateNewBlog from './components/CreateNewBlog';
 import { useDispatch, useSelector } from 'react-redux';
@@ -15,27 +14,6 @@ const App = () => {
     dispatch(getAllBlogs());
   }, []);
 
-  const handleUpdateLike = async (blog) => {
-    const updatedBlog = {
-      user: blog.user.id,
-      likes: blog.likes + 1,
-      author: blog.author,
-      title: blog.title,
-      url: blog.url,
-    };
-    try {
-      const response = await blogService.updateLike(blog.id, updatedBlog);
-      const updatedBlogs = blogs.map((el) => {
-        if (el.id === blog.id) {
-          return { ...el, likes: response.likes };
-        } else return el;
-      });
-      const sortedBlogs = blogService.sortBlogs(updatedBlogs);
-      setBlogs(sortedBlogs);
-    } catch (error) {
-      console.log(error.response.data.error);
-    }
-  };
   return (
     <>
       <LogIn>
