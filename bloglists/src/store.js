6eef36c186b67a266da5ff41baf94f338e21c97a
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-25 01:32:12 +0700

    Combine comment reducers to others

--- bloglists/src/store.js
+++ bloglists/src/store.js
@@ -3,11 +3,13 @@ import thunk from 'redux-thunk';
 import blogReducer from './reducers/blogReducer';
 import notificationReducer from './reducers/notificationReducer';
 import userReducer from './reducers/userReducer';
+import commentReducer from './reducers/commentReducer';
 
 const reducer = combineReducers({
   blogs: blogReducer,
   notification: notificationReducer,
   user: userReducer,
+  comments: commentReducer,
 });
 
 const store = createStore(reducer, applyMiddleware(thunk));
