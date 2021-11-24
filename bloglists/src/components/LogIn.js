f072214c6a716b7492500a4c3bee4ff6c7a5867b
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-24 23:30:55 +0700

    Modufy LogIn component

--- bloglists/src/components/LogIn.js
+++ bloglists/src/components/LogIn.js
@@ -3,13 +3,12 @@ import Notification from './Notification';
 import { useDispatch, useSelector } from 'react-redux';
 import { useField } from '../hooks';
 import { showNotification } from '../reducers/notificationReducer';
-import { login, logout } from '../reducers/userReducer';
+import { login } from '../reducers/userReducer';
 import userService from '../services/users';
 
-const Login = (props) => {
+const Login = () => {
   const username = useField('text');
   const password = useField('password');
-  const userData = useSelector(({ user }) => user.currentUser);
   const dispatch = useDispatch();
 
   const handleSubmit = async (e) => {
@@ -33,37 +32,19 @@ const Login = (props) => {
     }
   };
 
-  const handleLogout = () => {
-    dispatch(logout());
-  };
-
   return (
     <>
-      {userData.username ? (
-        <div>
-          <h2>blogs</h2>
-          <div>
-            <Notification />
-          </div>
-          <p>
-            {userData.name} logged in
-            <button onClick={handleLogout}>logout</button>
-          </p>
-          {props.children}
-        </div>
-      ) : (
-        <div>
-          <h1>log in to application</h1>
-          <Notification />
-          <form onSubmit={handleSubmit}>
-            username
-            <input id="username" name="username" {...username}></input>
-            password
-            <input id="password" name="password" {...password}></input>
-            <button type="submit">submit</button>
-          </form>
-        </div>
-      )}
+      <div>
+        <h1>log in to application</h1>
+        <Notification />
+        <form onSubmit={handleSubmit}>
+          username
+          <input id="username" name="username" {...username}></input>
+          password
+          <input id="password" name="password" {...password}></input>
+          <button type="submit">submit</button>
+        </form>
+      </div>
     </>
   );
 };
